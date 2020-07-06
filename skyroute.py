b7599f3a5b68c34b84cbddf49cbeda3c0b4dfe05
ntuan19 <61070900+ntuan19@users.noreply.github.com>
2020-07-06 01:42:03 -0700

    Update skyroute.py

--- skyroute.py
+++ skyroute.py
@@ -1,1 +1,8 @@
-from graph_search import 
+from graph_search import graph_search
+from vc_metro import vc_metro
+from vc_landmarks import vc_landmarks
+from landmark_choices import landmark_choices
+#landmark_string is a string connecting all of the landmarks together, each with its corresponding letter of the alphabet from landmark_choices on a new line.
+landmark_string = "" #as a string, we set it with nothing inside.
+#create a landmark_string:
+for letter,landmark in landmark_choices.items() 
